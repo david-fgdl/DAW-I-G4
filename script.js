2b53d201ed086bfb118514e06a1137eac37aa094
JuanmaRuizF <juanmaruizf@gmail.com>
2020-11-16 11:04:03 +0000

    Campos Obligatorios contacto, creacion json+script

---
+++ script.js
@@ -0,0 +1,11 @@
+function submitFunction() {
+    var x = document.getElementById("nombre");
+    var text = "Hola"
+    document.getElementById("nombre").innerHTML = text;
+}
+
+
+function sumaPrecios(){
+    var msg_final = "<p>Selected items: </p>"
+}
+
