4a2fa02e47baa036b9a87d90ceec2c073902caac
David Fernandez <david_fgdl@hotmail.com>
2020-11-28 01:43:57 +0000

    Carrousel JSON

--- script/eventosScript.js
+++ script/eventosScript.js
@@ -16,30 +16,79 @@ function eventosLoad(){
         const eventos = jsonObj['Eventos'];
 
         for (var i = 0; i < eventos.length; i++) {
-            const myArticle = document.createElement('article');
-            
-            const myH2 = document.createElement('h2');
+            myArticle = document.createElement('article');
+            myCarrousel = document.createElement('div');
+            myCarrousel.setAttribute("id", "carouselControls");
+            myCarrousel.setAttribute("class", "col-sm-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8 carouselImg carousel slide");
+            myCarrousel.setAttribute("data-ride", "carousel");
+
+            myH2 = document.createElement('h2');
             myH2.setAttribute("class", "evnt");
             myH2.textContent = eventos[i].nombreEvento;
             
-            const myPara = document.createElement('p');
-            myPara.setAttribute("class", "col-sm-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8")
+            myPara = document.createElement('p');
+            myPara.setAttribute("class", "text-justify col-sm-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8")
             myPara.textContent = eventos[i].descripcion;
             
-            const img1 = new Image();
+            myCarro = document.createElement('div');
+            myCarro.setAttribute("class", "carousel-inner");
+            
+            itemCarro1 = document.createElement('div');
+            itemCarro1.setAttribute("class", "carousel-item active");
+            img1 = new Image();
             img1.src = eventos[i].foto1;
-            const img2 = new Image();
+            img1.setAttribute("class", "d-block w-100");
+            itemCarro1.appendChild(img1);
+            myCarro.appendChild(itemCarro1);
+
+            itemCarro2 = document.createElement('div');
+            itemCarro2.setAttribute("class", "carousel-item");
+            img2 = new Image();
             img2.src = eventos[i].foto2;
-            const img3 = new Image();
+            img2.setAttribute("class", "d-block w-100");
+            itemCarro2.appendChild(img2);
+            myCarro.appendChild(itemCarro2);
+            
+            itemCarro3 = document.createElement('div');
+            itemCarro3.setAttribute("class", "carousel-item");
+            img3 = new Image();
             img3.src = eventos[i].foto3;
+            img3.setAttribute("class", "d-block w-100");
+            itemCarro3.appendChild(img3);
+            myCarro.appendChild(itemCarro3);
+
+            controlAnt = document.createElement('a');
+            controlAnt.setAttribute("class", "carousel-control-prev");
+            controlAnt.setAttribute("href", "#carouselControls");
+            controlAnt.setAttribute("role", "button");
+            controlAnt.setAttribute("data-slide", "prev");
+            myCarrousel.appendChild(controlAnt);
+
+            controlSig = document.createElement('a');
+            controlSig.setAttribute("class", "carousel-control-next");
+            controlSig.setAttribute("href", "#carouselControls");
+            controlSig.setAttribute("role", "button");
+            controlSig.setAttribute("data-slide", "next");
+            myCarrousel.appendChild(controlSig);
+            
+            /* botonAnt = document.createElement('span');
+            controlAnt.setAttribute("class", "carousel-control-prev-icon");
+            controlAnt.setAttribute("aria-hidden", "true");
+            controlAnt.appendChild(botonAnt);
+
+            botonSig = document.createElement('span');
+            botonSig.setAttribute("class", "carousel-control-next-icon");
+            botonSig.setAttribute("aria-hidden", "true");
+            controlAnt.appendChild(controlSig); */
+
             
             myArticle.appendChild(myH2);
             myArticle.appendChild(myPara);
-            myArticle.appendChild(img1);
-            myArticle.appendChild(img2);
-            myArticle.appendChild(img3);
-        
+            
+            myCarrousel.appendChild(myCarro);
+
             section.appendChild(myArticle);
+            section.appendChild(myCarrousel);
           }
       }
 }
