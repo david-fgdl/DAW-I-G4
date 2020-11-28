6913e68cd97671877bc57c127d9d628a70c0bc24
David Fernandez <david_fgdl@hotmail.com>
2020-11-28 00:17:47 +0000

    Carga correcta de JSon

--- script/eventosScript.js
+++ script/eventosScript.js
@@ -17,25 +17,27 @@ function eventosLoad(){
 
         for (var i = 0; i < eventos.length; i++) {
             const myArticle = document.createElement('article');
+            
             const myH2 = document.createElement('h2');
             myH2.setAttribute("class", "evnt");
-            const myPara1 = document.createElement('p');
-            const myPara2 = document.createElement('img');
-            const myPara3 = document.createElement('img');
-            const myPara4 = document.createElement('p');
-            
-        
             myH2.textContent = eventos[i].nombreEvento;
-            myPara1.textContent = eventos[i].descripcion;
-            myPara2.textContent = eventos[i].foto1;
-            myPara3.textContent = eventos[i].foto2;
-            myPara4.textContent = 'Precio: ' + eventos[i].precio + '€';
-                
+            
+            const myPara = document.createElement('p');
+            myPara.setAttribute("class", "col-sm-12 offset-md-1 col-md-10 offset-lg-2 col-lg-8")
+            myPara.textContent = eventos[i].descripcion;
+            
+            const img1 = new Image();
+            img1.src = eventos[i].foto1;
+            const img2 = new Image();
+            img2.src = eventos[i].foto2;
+            const img3 = new Image();
+            img3.src = eventos[i].foto3;
+            
             myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
-            myArticle.appendChild(myPara2);
-            myArticle.appendChild(myPara3);
-            myArticle.appendChild(myPara4);
+            myArticle.appendChild(myPara);
+            myArticle.appendChild(img1);
+            myArticle.appendChild(img2);
+            myArticle.appendChild(img3);
         
             section.appendChild(myArticle);
           }
